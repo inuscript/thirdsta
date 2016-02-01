65a2a23cd7a745d9061e0d42cd888a77b84d61ac
inuscript <inuscript@gmail.com>
2016-02-02 00:29:26 +0900

    websta

--- index.js
+++ index.js
@@ -1,7 +1,67 @@
-class Websta{
-  userPage(user){
-    return `http://websta.me/n/${user}`
+import axios from "axios"
+import cheerio from "cheerio"
+
+class WebstaRequest{
+  constructor(user){
+    this.user = user
+  }
+  userPageUrl(){
+    return `http://websta.me/n/${this.user}`
+  }
+  request(){
+    return axios(this.userPageUrl()).then(res => res.data)
   }
 }
-let w = new Websta()
-console.log(w.userPage("sqlatchdog"))
+
+class WebstaParser{
+  constructor(body){
+    this.body = body
+    this.$ = cheerio.load(this.body)
+  }
+  photos(){
+    return this.$(".photoeach").map((i, el) =>{
+      let p = new WebstaPhoto(el)
+      console.log(p.dump())
+      return p
+    })
+  }
+  parse(){
+    this.photos()
+  }
+}
+
+class WebstaPhoto{
+  constructor(dom){
+    this.$ = cheerio.load(dom)
+  }
+  get id(){
+    let url = this.$(".mainimg").attr("href")
+    return url.replace("/p/", "")
+  }
+  get like(){
+    return parseInt(this.$(`.like_count_${this.id}`).text())
+  }
+  get comment(){
+    return parseInt(this.$(`.comment_count_${this.id}`).text())
+  }
+  get tags(){
+    return this.$(".caption strong a").map( (i, el) => {
+      return this.$(el).text().replace(/^#/, "")
+    }).get()
+  }
+  dump(){
+    return {
+      id: this.id,
+      like: this.like,
+      comment: this.comment,
+      tags: this.tags
+    }
+  }
+}
+let w = new WebstaRequest("sqlatchdog")
+w.request().then(body => {
+  let parser = new WebstaParser(body)
+  parser.parse()
+}).catch(e => {
+  console.error(e)
+})
