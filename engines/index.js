fe3e1e7152f3acf13dac3283a6d02ddbe112a18b
inuscript <inuscript@gmail.com>
2016-02-04 20:33:53 +0900

    rename

--- engines/index.js
+++ engines/index.js
@@ -1,32 +1,34 @@
 import { WebstaRequest } from "./websta"
 import fs from "fs"
 
-const parse = (results, page, depth) =>{
+const reducePage = (results, page, depth) =>{
   return page.request().then( parser => {
     let nextPage = parser.next()
     let nextResults = results.concat(parser.parse())
     if(nextPage && depth < 3){
-      return parse(nextResults, nextPage, depth + 1)
+      return reducePage(nextResults, nextPage, depth + 1)
     }
     return nextResults
   })
 }
 
 const crawl = (req, userName, depth = 3) =>{
-  let results = []
   let page = req.startPage( userName )
-  return parse(results, page, 0)
+  return reducePage([], page, 0)
 }
 
-let userName = "sqlatchdog"
-let userId = "1453092205"
-let req = new WebstaRequest()
-// let req = new TofoRequest()
+const start = () => {
+  let userName = "sqlatchdog"
+  let userId = "1453092205"
+  let req = new WebstaRequest()
+  // let req = new TofoRequest()
 
-crawl(req, userName).then( item => {
-  let json = JSON.stringify(item, null, 2)
-  fs.writeFileSync("./foo.json", json)
-  console.log(item)
-}).catch(e => {
-  console.error(e.stack)
-})
+  crawl(req, userName).then( item => {
+    let json = JSON.stringify(item, null, 2)
+    fs.writeFileSync("./foo.json", json)
+    console.log(item)
+  }).catch(e => {
+    console.error(e.stack)
+  })
+}
+start()
