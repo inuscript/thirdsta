6b6269c849166b16a1593b2aaf7c73d49abddabc
inuscript <inuscript@gmail.com>
2016-02-06 15:46:48 +0900

    shrink

--- engines/index.js
+++ engines/index.js
@@ -1,7 +1,6 @@
 import Storage from "../storage/firebase"
 import websta from "./websta"
 import tofo from "./tofo"
-import fs from "fs"
 
 let storage = new Storage()
 
@@ -13,8 +12,6 @@ const store = (media) => {
 
 const reducePage = (results, page, depth) =>{
   return page.request().then( parser => {
-    // console.log(depth)
-    // console.log(page.url)
     let nextPage = parser.next()
     let media = parser.parse()
     let stores = store(media)
@@ -24,7 +21,6 @@ const reducePage = (results, page, depth) =>{
       return reducePage(nextResults, nextPage, depth + 1)
     }
     return Promise.all(stores).then( (results) => {
-      // console.log(results)
       return nextResults
     })
   })
@@ -41,9 +37,6 @@ const crawl = ( depth = 10 ) =>{
 
 export default const = () => {
   crawl().then( item => {
-    let json = JSON.stringify(item, null, 2)
-    // fs.writeFileSync("./foo.json", json)
-    // console.log(item)
     process.exit(0)
   }).catch(e => {
     console.error(e.stack)
